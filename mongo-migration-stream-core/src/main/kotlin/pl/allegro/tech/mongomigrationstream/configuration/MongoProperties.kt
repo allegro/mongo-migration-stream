5e927ee507023de1cec7fd8f292b655b6e725518
Szymon Marcinkiewicz <szymon.a.marcinkiewicz@allegro.com>
2024-04-12 10:53:36 +0200

    Configurable mongo timeout (#70)

--- mongo-migration-stream-core/src/main/kotlin/pl/allegro/tech/mongomigrationstream/configuration/MongoProperties.kt
+++ mongo-migration-stream-core/src/main/kotlin/pl/allegro/tech/mongomigrationstream/configuration/MongoProperties.kt
@@ -1,9 +1,14 @@
 package pl.allegro.tech.mongomigrationstream.configuration
 
+import java.time.Duration
+
+private val DEFAULT_MONGO_TIMEOUT = Duration.ofSeconds(30)
+
 data class MongoProperties(
     val uri: String,
     val dbName: String,
-    val authenticationProperties: MongoAuthenticationProperties? = null
+    val authenticationProperties: MongoAuthenticationProperties? = null,
+    val timeoutProperties: MongoTimeoutProperties = MongoTimeoutProperties.DEFAULT, // TODO: Add possibility to read those values from properties file
 ) {
     override fun toString(): String = uri
 
@@ -12,4 +17,18 @@ data class MongoProperties(
         val password: String,
         val authDbName: String
     )
+
+    data class MongoTimeoutProperties(
+        val connectTimeout: Duration,
+        val readTimeout: Duration,
+        val serverSelectionTimeout: Duration,
+    ) {
+        companion object {
+            val DEFAULT = MongoTimeoutProperties(
+                connectTimeout = DEFAULT_MONGO_TIMEOUT,
+                readTimeout = DEFAULT_MONGO_TIMEOUT,
+                serverSelectionTimeout = DEFAULT_MONGO_TIMEOUT,
+            )
+        }
+    }
 }
