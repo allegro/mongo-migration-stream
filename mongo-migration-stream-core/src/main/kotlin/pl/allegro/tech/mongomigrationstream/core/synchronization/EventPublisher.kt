73dfb868d0262caecfd1b4423e7bae6178dd4011
Michal Knasiecki <michal.knasiecki@allegro.pl>
2024-04-18 10:45:29 +0200

    loggin events

--- mongo-migration-stream-core/src/main/kotlin/pl/allegro/tech/mongomigrationstream/core/synchronization/EventPublisher.kt
+++ mongo-migration-stream-core/src/main/kotlin/pl/allegro/tech/mongomigrationstream/core/synchronization/EventPublisher.kt
@@ -3,7 +3,10 @@ package pl.allegro.tech.mongomigrationstream.core.synchronization
 import com.mongodb.bulk.BulkWriteResult
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.model.BulkWriteOptions
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.bson.BsonDocument
+
+private val logger = KotlinLogging.logger { }
 
 internal class EventPublisher(
     private val destinationCollection: MongoCollection<BsonDocument>
@@ -11,6 +14,16 @@ internal class EventPublisher(
 
     internal fun publishBulkEvents(events: List<ChangeEvent>): BulkWriteResult {
         val bulkEvents = events.mapNotNull { it.toWriteModel() }
-        return destinationCollection.bulkWrite(bulkEvents, BulkWriteOptions().ordered(true))
+        logger.info{"Event: [$events] mapped to: [$bulkEvents]"}
+        return BulkWriteResult.unacknowledged()
+
+//        try {
+//            val result = destinationCollection.bulkWrite(bulkEvents, BulkWriteOptions().ordered(true))
+//            logger.info{"SUCCESS: [$events] mapped to: [$bulkEvents]"}
+//            return result
+//        } catch (e: Exception) {
+//            logger.error{"FAILED: [$events] mapped to: [$bulkEvents]"}
+//            return BulkWriteResult.unacknowledged()
+//        }
     }
 }
