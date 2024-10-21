13c64fcd66608d2a0a76a1c146e44b577148c0a7
Szymon Marcinkiewicz <szymon.a.marcinkiewicz@allegro.com>
2024-10-21 15:06:01 +0200

    Fix ktlint

--- mongo-migration-stream-core/src/main/kotlin/pl/allegro/tech/mongomigrationstream/infrastructure/mongo/MongoDbClients.kt
+++ mongo-migration-stream-core/src/main/kotlin/pl/allegro/tech/mongomigrationstream/infrastructure/mongo/MongoDbClients.kt
@@ -1,18 +1,18 @@
 package pl.allegro.tech.mongomigrationstream.infrastructure.mongo
 
-import com.mongodb.reactivestreams.client.MongoClient as ReactiveMongoClient
-import com.mongodb.reactivestreams.client.MongoDatabase as ReactiveMongoDatabase
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoDatabase
 import io.micrometer.core.instrument.MeterRegistry
 import pl.allegro.tech.mongomigrationstream.configuration.ApplicationProperties
 import pl.allegro.tech.mongomigrationstream.configuration.MongoProperties
+import com.mongodb.reactivestreams.client.MongoClient as ReactiveMongoClient
+import com.mongodb.reactivestreams.client.MongoDatabase as ReactiveMongoDatabase
 
 private const val CONFIG_DB = "config"
 
 internal class MongoDbClients(
     properties: ApplicationProperties,
-    meterRegistry: MeterRegistry
+    meterRegistry: MeterRegistry,
 ) {
     private val sourceClient: MongoClient =
         createDbClient(properties.sourceDbProperties, meterRegistry)
@@ -33,11 +33,15 @@ internal class MongoDbClients(
     val reactiveDestinationDatabase: ReactiveMongoDatabase =
         reactiveDestinationClient.getDatabase(properties.destinationDbProperties.dbName)
 
-    private fun createDbClient(mongoProperties: MongoProperties, meterRegistry: MeterRegistry): MongoClient =
-        MongoClientFactory.buildClient(mongoProperties, meterRegistry)
+    private fun createDbClient(
+        mongoProperties: MongoProperties,
+        meterRegistry: MeterRegistry,
+    ): MongoClient = MongoClientFactory.buildClient(mongoProperties, meterRegistry)
 
-    private fun createReactiveDbClient(mongoProperties: MongoProperties, meterRegistry: MeterRegistry): ReactiveMongoClient =
-        MongoClientFactory.buildReactiveClient(mongoProperties, meterRegistry)
+    private fun createReactiveDbClient(
+        mongoProperties: MongoProperties,
+        meterRegistry: MeterRegistry,
+    ): ReactiveMongoClient = MongoClientFactory.buildReactiveClient(mongoProperties, meterRegistry)
 
     fun closeClients() {
         sourceClient.close()
